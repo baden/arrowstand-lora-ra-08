b5afead2af8a6b1d858629cf6a130dc90277f2af
Denys Batrak <baden.i.ua@gmail.com>
2025-01-16 01:21:37 +0200

    I2C через DMA теж не працює. От сука!

--- src/main.c
+++ src/main.c
@@ -6,12 +6,13 @@
 #include "board_led.h"
 #include "oled_min.h"
 
-#define TEST_GPIOX GPIOA
-#define TEST_PIN   GPIO_PIN_9
+// #define TEST_GPIOX GPIOA
+// #define TEST_PIN   GPIO_PIN_9
 
 static void uart_log_init(void)
 {
     rcc_enable_peripheral_clk(RCC_PERIPHERAL_UART0, true);
+
     // uart0
     gpio_set_iomux(GPIOB, GPIO_PIN_0, 1);
     gpio_set_iomux(GPIOB, GPIO_PIN_1, 1);
@@ -28,12 +29,14 @@ static void uart_log_init(void)
 static void board_init()
 {
     rcc_enable_peripheral_clk(RCC_PERIPHERAL_GPIOA, true);
-    gpio_init(TEST_GPIOX, TEST_PIN, GPIO_MODE_OUTPUT_PP_HIGH);
+    rcc_enable_peripheral_clk(RCC_PERIPHERAL_GPIOB, true);
+    
+    rcc_enable_peripheral_clk(RCC_PERIPHERAL_SYSCFG, true);     // (З прикладу I2C master dma, треба глянути для чого це)
+    // gpio_init(TEST_GPIOX, TEST_PIN, GPIO_MODE_OUTPUT_PP_HIGH);
     // rcc_enable_oscillator(RCC_OSC_XO32K, true);
 
     // LOG
     // rcc_enable_peripheral_clk(RCC_PERIPHERAL_GPIOA, true);
-    rcc_enable_peripheral_clk(RCC_PERIPHERAL_GPIOB, true);
     // rcc_enable_peripheral_clk(RCC_PERIPHERAL_GPIOC, true);
     // rcc_enable_peripheral_clk(RCC_PERIPHERAL_GPIOD, true);
     // rcc_enable_peripheral_clk(RCC_PERIPHERAL_PWR, true);
@@ -72,7 +75,7 @@ int main(void)
 
         board_led_rgb(counter & (1<<0), counter & (1<<1), counter & (1<<2));
 
-        gpio_toggle(TEST_GPIOX, TEST_PIN);
+        // gpio_toggle(TEST_GPIOX, TEST_PIN);
     }
 }
 
